b8c33685adb473c2bc715a61172d538b40123f10
John <Johnazzi430@gmail.com>
2019-12-02 00:43:01 -0500

    v3 didnt work. trying to add wsgi

--- server/wsgi.py
+++
@@ -1,2 +0,0 @@
-from src.application import create_app
-app = create_app()
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
-import os
 from server import app
 
 application = app
 
 if __name__ == "__main__":
 #    app.run()
-    application.run(host='0.0.0.0' , port=5000)
+    application.run(host='0.0.0.0' , port='8080')
