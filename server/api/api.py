4672bc42c29715a904e637c670693b7ffc5f903b
johnazzi430 <johnazzi430@gmail.com>
2019-12-10 11:38:14 -0500

    Add files via upload

--- server/api/api.py
+++ server/api/api.py
@@ -15,9 +15,6 @@ api = Blueprint('api_bp', __name__,url_prefix='/api')
 
 
 
-
-
-
 ## ---------------- SERVE STATIC
 #
 # @api.route('/', defaults={'path': ''})
@@ -461,7 +458,7 @@ def get_user_data():
 def admin_user():
     username = request.json.get('username')
     new_role = request.json.get('role')
-    if request.json.get('admin') == 'admin':
+    if request.json.get('admin').lower() == 'admin':
         with sqlite3.connect('server/data/data.db') as conn:
             c = conn.cursor()
             c.execute("UPDATE USERS SET role = ? WHERE username = ?", [ new_role, username])
